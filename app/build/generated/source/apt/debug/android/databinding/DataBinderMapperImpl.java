2cf674dcfd7f3822847ed2b9a8338afafee8e5b4
soumgraphic <soumgraphic@gmail.com>
2018-12-08 16:56:54 +0100

    .gitignore fix

--- app/build/generated/source/apt/debug/android/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package android.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new dev.soumgraphic.com.toumsprofile.DataBinderMapperImpl());
-  }
-}
